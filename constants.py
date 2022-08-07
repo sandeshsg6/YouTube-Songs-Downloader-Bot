3be709291fafca8827ed3ed53a6a7aa97bb0553c
Sandesh <sandeshsg6@gmail.com>
2022-08-07 08:15:55 +0530

    ready for public

---
+++ constants.py
@@ -0,0 +1,3 @@
+welcome_text = 'Hi!\nWelcome to You Tube Downloader Bot.\nJust send a Youtube link to me and I will download it and send it as a video!'
+reply_to_text_message = 'Hey, I\'m not designed to reply to text messages 😕\nHit /start and read the instructions!'
+unable_to_download = "Please enter a valid Youtube link "
---
+++ ytbot.py
@@ -0,0 +1,51 @@
+import logging
+from telegram import Update, ForceReply,bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import os
+import constants
+from pytube import YouTube
+import validators
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(constants.welcome_text)
+
+def download_videos(update: Update, context: CallbackContext):
+    if validators.url(update.message.text):
+        try:
+            update.message.reply_text("Downloading your video!")
+            video_path = YouTube(update.message.text).streams.get_highest_resolution().download()
+            update.message.reply_text("video downloaded. Uploading it!") 
+            context.bot.send_video(update.message.chat_id, video=open(str(video_path), 'rb'))
+            os.remove(video_path)
+        except:
+           update.message.reply_text(constants.unable_to_download) 
+    else:
+        update.message.reply_text(constants.reply_to_text_message)
+    
+def main() -> None:
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    bot_token = os.environ.get("BOT_TOKEN","")
+    updater = Updater(bot_token, use_context=True) 
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, download_videos))
+
+    # Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
