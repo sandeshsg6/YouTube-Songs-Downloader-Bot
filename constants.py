ca903c36646efbbec3aff91a00aee957c8e2ea13
Sandesh <sandeshsg6@gmail.com>
2022-08-09 10:34:28 +0530

    updated constants

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-welcome_text = 'Hi!\nWelcome to You Tube Downloader Bot.\nJust send a Youtube link to me and I will download it and send it as a video!'
+welcome_text = 'Hi!\nWelcome to You Tube Songs Downloader Bot.\nJust send a Youtube link to me and I will download the song and send it to you!'
 reply_to_text_message = 'Hey, I\'m not designed to reply to text messages 😕\nHit /start and read the instructions!'
 unable_to_download = "Please enter a valid Youtube link "
