7471cc44bcd482e58432fdfaf2bed288da6a35af
Sandesh <sandeshsg6@gmail.com>
2022-08-10 11:09:39 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
-welcome_text = 'Hi!\nWelcome to You Tube Songs Downloader Bot.\nJust send a Youtube link to me and I will download the song and send it to you!'
+welcome_text = 'Hi!\nWelcome to You Tube Songs Downloader 📥 Bot.\nJust send a Youtube link to me and I will download the 🎵 and send it to you!'
 reply_to_text_message = 'Hey, I\'m not designed to reply to text messages 😕\nHit /start and read the instructions!'
 unable_to_download = "❌ Found Nothing.\nTry another link or use @vid song_name."
